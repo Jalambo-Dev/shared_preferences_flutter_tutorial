7213785f71fe7d50fec229824b2379d3b3b63cee
Jalambo-Dev <ahmedjalambo.dev@gmail.com>
2025-02-13 11:48:10 +0200

    Implement theme toggle functionality in HomeScreen and update MyApp to manage dark mode state

--- theme_toggle_with_shared_pref/lib/home_screen.dart
+++ theme_toggle_with_shared_pref/lib/home_screen.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final bool isDarkMode;
+  final void Function() toogleTheme;
+
+  const HomeScreen({
+    super.key,
+    required this.isDarkMode,
+    required this.toogleTheme,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Hello World !')),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shared Preferences Theme Toggle'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 18,
+          children: [
+            Text('Press to change theme  :'),
+            IconButton(
+              onPressed: () {
+                toogleTheme();
+              },
+              icon: Icon(
+                isDarkMode ? Icons.dark_mode : Icons.light_mode,
+                size: 42,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- theme_toggle_with_shared_pref/lib/main.dart
+++ theme_toggle_with_shared_pref/lib/main.dart
@@ -5,17 +5,27 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDarkMode = false;
+
+  void _toogleTheme() {
+    setState(() {
+      isDarkMode = !isDarkMode;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomeScreen(),
+      theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: HomeScreen(isDarkMode: isDarkMode, toogleTheme: _toogleTheme),
     );
   }
 }
